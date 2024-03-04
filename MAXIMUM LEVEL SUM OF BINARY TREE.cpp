fa4b1f81ff126e9d2a2e4a11e1b6102862623e66
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2024-03-04 21:12:09 +0530

    Add files via upload

---
+++ MAXIMUM LEVEL SUM OF BINARY TREE.cpp
@@ -0,0 +1,46 @@
+class Solution
+{
+public:
+     int maxLevelSum(TreeNode *root)
+     {
+          int maxSum = INT_MIN;
+          int resultLevel = 0;
+          int currLevel = 1;
+
+          queue<TreeNode *> que;
+          que.push(root);
+
+          while (!que.empty())
+          {
+
+               int n = que.size();
+
+               int sum = 0;
+
+               while (n--)
+               {
+
+                    TreeNode *node = que.front();
+                    que.pop();
+
+                    sum += node->val;
+
+                    if (node->left)
+                         que.push(node->left);
+
+                    if (node->right)
+                         que.push(node->right);
+               }
+
+               if (sum > maxSum)
+               {
+                    maxSum = sum;
+                    resultLevel = currLevel;
+               }
+               currLevel++;
+          }
+
+          return resultLevel;
+     }
+};
+// LEETCODE PROBLEM -  1161
