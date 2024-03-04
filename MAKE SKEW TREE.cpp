6663c521792660da30d1587ba0f770291573451b
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2024-03-04 23:06:54 +0530

    Add files via upload

---
+++ MAKE SKEW TREE.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+     TreeNode *increasingBST(TreeNode *root, TreeNode *nxt = NULL)
+     {
+          if (!root)
+               return nxt;
+          root->right = increasingBST(root->right, nxt);
+          TreeNode *temp = increasingBST(root->left, root);
+          root->left = NULL;
+          return temp;
+     }
+};
+
+// LEETCODE PROBLEM - 897
