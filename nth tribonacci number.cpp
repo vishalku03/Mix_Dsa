eff3ea8bdc4cb69ab850af530894f004eac5f0a2
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2024-04-24 19:37:29 +0530

    Add files via upload

---
+++ nth tribonacci number.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+     int tribonacci(int n)
+     {
+          if (n <= 1)
+               return n;
+          if (n == 2)
+               return 1;
+
+          int prev1 = 1, prev2 = 1, prev3 = 0;
+          for (int i = 3; i <= n; i++)
+          {
+               int curr = prev1 + prev2 + prev3;
+
+               prev3 = prev2;
+               prev2 = prev1;
+               prev1 = curr;
+          }
+
+          return prev1;
+     }
+};
+// leetcode - 1137
