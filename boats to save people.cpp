d47116acceae471fd9761861210b6c4ecad84d36
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2024-05-04 19:38:10 +0530

    Add files via upload

---
+++ boats to save people.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+     int numRescueBoats(vector<int> &people, int limit)
+     {
+
+          sort(people.begin(), people.end());
+          int i = 0;
+          int boats = 0;
+          int j = people.size() - 1;
+
+          while (i <= j)
+          {
+               if (people[i] + people[j] <= limit)
+               {
+                    i++;
+                    j--;
+                    boats++;
+               }
+               else
+               {
+                    boats++;
+                    j--;
+               }
+          }
+
+          return boats;
+     }
+};
+//  leetcode  = 881
